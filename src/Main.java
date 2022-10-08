53eb33160080a684d40312828668f5df3279fc4e
Maxim <froshig@gmail.com>
2022-10-08 21:00:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 5000;
+        int x = 20;
+        int result = amount / x;
+        System.out.println(result + " миль");
+    }
+}
